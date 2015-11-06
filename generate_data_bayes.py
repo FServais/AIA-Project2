8f195472421dcc1d5b7e3b979c1f08a9f83e4175
Fabrice Servais <fabrice.servais@gmail.com>
2015-11-06 17:19:31 +0100

    Correct generate data

--- generate_data_bayes.py
+++ generate_data_bayes.py
@@ -2,18 +2,19 @@ from sklearn.utils import check_random_state
 import numpy as np
 
 
-def generate_data(n_samples, r1, r2, sigma, random_state=None):
+def generate_data(n_samples, r1, r2, sigma, random_state=0):
     random_state = check_random_state(random_state)
 
     y = random_state.randint(0, 2, size=n_samples)
 
     X = np.zeros((n_samples ,2))
+
     for i in range(0, n_samples):
         alpha = random_state.uniform(low=0.0, high=2*np.pi)
 
         rand_bit = random_state.randint(0, 2)
 
-        r = random_state.normal(loc=(1 + rand_bit)*(r1 if y[i] == 0 else r2), scale=sigma**(1 + rand_bit))
+        r = random_state.normal(loc=(1 + rand_bit)*(r1 if y[i] == 0 else r2), scale=sigma)
 
         X[i, 0] = r * np.cos(alpha)
         X[i, 1] = r * np.sin(alpha)
