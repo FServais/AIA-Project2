f8f494f5b744b7b9606a7af60929df5d717afb4d
Fabrice Servais <fabrice.servais@gmail.com>
2015-11-17 21:00:34 +0100

    2 types of integration

--- question1.py
+++ question1.py
@@ -1,3 +1,5 @@
+from scipy import integrate
+
 import numpy as np
 from matplotlib import pyplot as plt
 from pylab import meshgrid
@@ -10,6 +12,8 @@ def prob_r_fixed_y(r, R, sigma):
 def normal_distrib(x, mu, sigma):
     return (1/(sigma * np.sqrt(2 * np.pi))) * np.exp((- (x - mu)**2)/(2 * sigma**2))
 
+def normal_exp(x, mu, sigma):
+    return np.exp((- (x - mu)**2)/(2 * sigma**2))
 
 def save_2_var_plot(x, y, z, name="file.png", xlabel="x", ylabel="y", title="Title"):
     plt.figure()
@@ -20,6 +24,62 @@ def save_2_var_plot(x, y, z, name="file.png", xlabel="x", ylabel="y", title="Tit
     plt.colorbar()
     plt.savefig(name)
     plt.close()
+
+def generalisation_error(R1, R2, sigma):
+    # Predict 1
+    A0 = 0
+    A1 = (R1+R2)/2
+
+    B0 = (R1+2*R2)/2
+    B1 = R1+R2
+
+    # Predict 0
+    C0 = A1
+    C1 = B0
+
+    D0 = B1
+    D1 = 10**9
+
+    term_y_0_1 = normal_exp(C1, R1, sigma) - normal_exp(C0, R1, sigma)
+    term_y_0_2 = normal_exp(D1, R1, sigma) - normal_exp(D0, R1, sigma)
+    term_y_0_3 = normal_exp(C1, 2*R1, sigma) - normal_exp(C0, 2*R1, sigma)
+    term_y_0_4 = normal_exp(D1, 2*R1, sigma) - normal_exp(D0, 2*R1, sigma)
+
+    term_y_1_1 = normal_exp(A1, R2, sigma) - normal_exp(A0, R2, sigma)
+    term_y_1_2 = normal_exp(B1, R2, sigma) - normal_exp(B0, R2, sigma)
+    term_y_1_3 = normal_exp(A1, 2*R2, sigma) - normal_exp(A0, 2*R2, sigma)
+    term_y_1_4 = normal_exp(B1, 2*R2, sigma) - normal_exp(B0, 2*R2, sigma)
+
+    return 1 - (1/2) * (-(sigma**2)/np.sqrt(2 * np.pi * sigma**2)) * (term_y_0_1 + term_y_0_2 + term_y_0_3 + term_y_0_4 + term_y_1_1 + term_y_1_2 + term_y_1_3 + term_y_1_4)
+
+
+def generalisation_error_int(R1, R2, sigma):
+    # Predict 1
+    A0 = 0
+    A1 = (R1+R2)/2
+
+    B0 = (R1+2*R2)/2
+    B1 = R1+R2
+
+    # Predict 0
+    C0 = A1
+    C1 = B0
+
+    D0 = B1
+    D1 = np.inf
+
+    err = [0, 0, 0, 0]
+
+    int_y_0_A, err[0] = integrate.quad(lambda x: x * prob_r_fixed_y(x, R1, sigma), C0, C1)
+    int_y_0_B, err[1] = integrate.quad(lambda x: x * prob_r_fixed_y(x, R1, sigma), D0, D1)
+
+    int_y_1_A, err[2] = integrate.quad(lambda x: x * prob_r_fixed_y(x, R2, sigma), A0, A1)
+    int_y_1_B, err[3] = integrate.quad(lambda x: x * prob_r_fixed_y(x, R2, sigma), B0, B1)
+
+    print("Mean error: {}".format(np.mean(err)))
+    print("{} + {} + {} + {}".format(int_y_0_A, int_y_0_B, int_y_1_A, int_y_1_B))
+
+    return 1 - 0.5 * (int_y_0_A + int_y_0_B + int_y_1_A + int_y_1_B)
 
 if __name__ == "__main__":
     X = np.linspace(0, 2, num=200)
@@ -88,3 +148,6 @@ if __name__ == "__main__":
     plt.title("Normal distributions of P(r | y=C), for C = {0,1}")
     plt.savefig("compare.png")
     plt.close()
+
+    print("Generalisation error (integral): {}".format(generalisation_error_int(R1, R2, sigma)))
+    print("Generalisation error (hand): {}".format(generalisation_error(R1, R2, sigma)))
