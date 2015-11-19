63d67174095803f86203c47c0bbb966380737c35
Fabrice Servais <fabrice.servais@gmail.com>
2015-11-19 23:25:27 +0100

    Clean Q1

--- question1.py
+++ question1.py
@@ -6,60 +6,36 @@ from pylab import meshgrid
 
 from generate_data_bayes import generate_data
 
-def cart2pol(x, y):
-    rho = np.sqrt(x**2 + y**2)
-    phi = np.arctan2(y, x)
-    return(rho, phi)
 
-def prob_r(r, R, sigma):
-    return 0.5 * (normal_distrib(r, R, sigma) + normal_distrib(r, 2*R, sigma))
+def cart2pol(x, y):
+    """
+    Convert cartesian coordinates into polar coordinates.
+    """
+    rho = np.sqrt(x**2 + y**2)
+    theta = np.arctan2(y, x)
+    return (rho, theta)
 
 
 def normal_distrib(x, mu, sigma):
+    """
+    Value of the probability for a normal distribution N(mu, sigma**2).
+    """
     return (1/(sigma * np.sqrt(2 * np.pi))) * np.exp((- (x - mu)**2)/(2 * sigma**2))
 
-def normal_exp(x, mu, sigma):
-    return np.exp((- (x - mu)**2)/(2 * sigma**2))
 
-def save_2_var_plot(x, y, z, name="file.png", xlabel="x", ylabel="y", title="Title"):
-    plt.figure()
-    plt.pcolor(x, y, z)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.title(title)
-    plt.colorbar()
-    plt.savefig(name)
-    plt.close()
+def prob_r(r, R, sigma):
+    """
+    Probability P(r|y=C_i)
+    """
+    return 0.5 * (normal_distrib(r, R, sigma) + normal_distrib(r, 2*R, sigma))
 
-# def generalisation_error(R1, R2, sigma):
-#     # Predict 1
-#     A0 = 0
-#     A1 = (R1+R2)/2
-#
-#     B0 = (R1+2*R2)/2
-#     B1 = R1+R2
-#
-#     # Predict 0
-#     C0 = A1
-#     C1 = B0
-#
-#     D0 = B1
-#     D1 = 10**9
-#
-#     term_y_0_1 = normal_exp(C1, R1, sigma) - normal_exp(C0, R1, sigma)
-#     term_y_0_2 = normal_exp(D1, R1, sigma) - normal_exp(D0, R1, sigma)
-#     term_y_0_3 = normal_exp(C1, 2*R1, sigma) - normal_exp(C0, 2*R1, sigma)
-#     term_y_0_4 = normal_exp(D1, 2*R1, sigma) - normal_exp(D0, 2*R1, sigma)
-#
-#     term_y_1_1 = normal_exp(A1, R2, sigma) - normal_exp(A0, R2, sigma)
-#     term_y_1_2 = normal_exp(B1, R2, sigma) - normal_exp(B0, R2, sigma)
-#     term_y_1_3 = normal_exp(A1, 2*R2, sigma) - normal_exp(A0, 2*R2, sigma)
-#     term_y_1_4 = normal_exp(B1, 2*R2, sigma) - normal_exp(B0, 2*R2, sigma)
-#
-#     return 1 - (1/2) * (-(sigma**2)/np.sqrt(2 * np.pi * sigma**2)) * (term_y_0_1 + term_y_0_2 + term_y_0_3 + term_y_0_4 + term_y_1_1 + term_y_1_2 + term_y_1_3 + term_y_1_4)
-#
 
 def predict_bayes(X):
+    """
+    Prediction using the Bayes model.
+    :param X: [x1 x2], cartesian coordinates
+    :return: Value of the class, either 0 or 1.
+    """
     r, _ = cart2pol(X[0], X[1])
 
     if 0 <= r < (R1+R2)/2:
@@ -72,8 +48,10 @@ def predict_bayes(X):
         return 0
 
 
-
-def generalisation_error_int(R1, R2, sigma):
+def generalisation_error(R1, R2, sigma):
+    """
+    Compute the generalisation error.
+    """
     # Predict 1
     A0 = 0
     A1 = (R1+R2)/2
@@ -88,18 +66,24 @@ def generalisation_error_int(R1, R2, sigma):
     D0 = B1
     D1 = np.inf
 
-    err = [0, 0, 0, 0]
+    int_y_0_A, _ = integrate.quad(lambda r: prob_r(r, R1, sigma), C0, C1)
+    int_y_0_B, _ = integrate.quad(lambda r: prob_r(r, R1, sigma), D0, D1)
 
-    int_y_0_A, err[0] = integrate.quad(lambda r: prob_r(r, R1, sigma), C0, C1)
-    int_y_0_B, err[1] = integrate.quad(lambda r: prob_r(r, R1, sigma), D0, D1)
-
-    int_y_1_A, err[2] = integrate.quad(lambda r: prob_r(r, R2, sigma), A0, A1)
-    int_y_1_B, err[3] = integrate.quad(lambda r: prob_r(r, R2, sigma), B0, B1)
-
-    print("Mean error: {}".format(np.mean(err)))
-    print("{} + {} + {} + {}".format(int_y_0_A, int_y_0_B, int_y_1_A, int_y_1_B))
+    int_y_1_A, _ = integrate.quad(lambda r: prob_r(r, R2, sigma), A0, A1)
+    int_y_1_B, _ = integrate.quad(lambda r: prob_r(r, R2, sigma), B0, B1)
 
     return 1 - 0.5 * (int_y_0_A + int_y_0_B + int_y_1_A + int_y_1_B)
+
+
+def save_2_var_plot(x, y, z, name="file.png", xlabel="x", ylabel="y", title="Title"):
+    plt.figure()
+    plt.pcolor(x, y, z)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.colorbar()
+    plt.savefig(name)
+    plt.close()
 
 if __name__ == "__main__":
     _r = np.linspace(0, 2, num=200)
@@ -110,6 +94,9 @@ if __name__ == "__main__":
     R1 = 0.7
     R2 = 0.5
     sigma = 0.1
+
+    # ------------------------------------
+    # Plotting P(r | y=0), P(r | y=1) and the associated Bayes model
 
     # P(r | y=0)
     P0 = prob_r(r, R1, sigma)
@@ -129,7 +116,9 @@ if __name__ == "__main__":
     save_2_var_plot(r, theta, P1, "P1.png", "r", "theta", "P(r | y=1)")
     save_2_var_plot(r, theta, C, "h.png", "r", "theta", "h(r, theta)")
 
-    # Plot the normal distributions
+    # ------------------------------------
+    # Plot the normal distributions and their intersections
+
     x = np.linspace(0, 2.5, num=200)
     plt.figure()
     # P(r | y=0)
@@ -152,27 +141,13 @@ if __name__ == "__main__":
     plt.savefig("Normal_distributions.png")
     plt.close()
 
+    # ------------------------------------
+    # Computing the generalisation error
 
-    # Plot the comparison between normal distributions and the sum sums of the distributions
-    x = np.linspace(0, 2.5, num=200)
-    plt.figure()
-    # P(r | y=0)
-    plt.plot(x, prob_r(x, R1, sigma))
+    # Integration
+    print("Generalisation error (integration): {}".format(generalisation_error(R1, R2, sigma)))
 
-    plt.plot(x, normal_distrib(x, R1, sigma))
-    plt.plot(x, normal_distrib(x, 2*R1, sigma))
-
-    plt.legend(["P(r | y=0)", "P(r | y=1)", "N(R1)", "N(R2)"])
-    plt.xlabel("x")
-    plt.ylabel("P")
-    plt.title("Normal distributions of P(r | y=C), for C = {0,1}")
-    plt.savefig("compare.png")
-    plt.close()
-
-    print("Generalisation error (integral): {}".format(generalisation_error_int(R1, R2, sigma)))
-    # print("Generalisation error (hand): {}".format(generalisation_error(R1, R2, sigma)))
-
-    # Experiment
+    # Experimental
     error = 0
     n_samples = 1500
     X, y = generate_data(n_samples, R1, R2, sigma)
@@ -182,4 +157,4 @@ if __name__ == "__main__":
         if y_predict != y[s]:
             error += 1
 
-    print("Error rate: {}".format(error/n_samples))
+    print("Generalisation error (experimental): {}".format(error/n_samples))
